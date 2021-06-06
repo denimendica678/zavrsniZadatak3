3a5fc59364f35cd898f6bed5d4bfeea2048cadc9
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-06 12:26:06 +0200

    Add files via upload

---
+++ zavrsniZadatak3/zavrsniZadatak3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsniZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cetiri, sest, cetiriIsest, broj;
+
+            Console.Write("Molimo Vas unesite broj: ");
+            broj = Convert.ToInt32(Console.ReadLine());
+
+            if(broj % 6 == 0 && broj % 4 != 0)
+            {
+                Console.Write("Uneseni broj djeljiv je sa 6.");
+            }
+
+            if (broj % 4 == 0 && broj % 6 != 0)
+            {
+                Console.Write("Uneseni broj djeljiv je sa 4.");
+            }
+
+
+            if (broj % 6 == 0 && broj % 4 == 0)
+            {
+                Console.Write("Uneseni broj djeljiv je sa 6 i 4.");
+            }
+            
+            if (broj % 6 != 0 && broj % 4 != 0)
+            {
+                Console.Write("Uneseni broj nije djeljiv sa 6 i 4.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
